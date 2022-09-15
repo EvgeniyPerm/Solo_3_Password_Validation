ca79f147d202537572d89c4a2654d41f3e25e369
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-15 13:07:01 +0500

    You're interviewing to join a security team. They want to see you build a password evaluator for your technical interview to validate the input.
    Task:
    Write a program that takes in a string as input and evaluates it as a valid password. The password is valid if it has at a minimum 2 numbers, 2 of the following special characters ('!', '@', '#', '$', '%', '&', '*'), and a length of at least 7 characters.
    If the password passes the check, output 'Strong', else output 'Weak'.
    
    Input Format:
    A string representing the password to evaluate.
    
    Output Format:
    A string that says 'Strong' if the input meets the requirements, or 'Weak', if not.
    
    Sample Input:
    Hello@$World19
    
    Sample Output:
    Strong

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
+class Password {
+    private String pass;
+    public String special = "!@#$%&*";
+    public String getPass(){
+        return pass;
+    }
+    public void SetPass(String password){
+        pass = password;
+    }
+    Password(String password){
+        this.pass=password;
+    }
+    public boolean isStrong(){
+        if (pass.length()<7) return false;
+        int num = 0;
+        int spec = 0;
+        for (int i = 0; i < pass.length(); i++) {
+            if (Character.isDigit(pass.charAt(i))) {
+                num++;
+                continue;
+            }
+            if (special.contains(""+pass.charAt(i))) {
+                spec++;
+            }
+        }
+        if (num>=2 && spec>=2) return true;
+        return false;
+
+    }
+}
+
 public class Main {
+    public static final String STRONG = "Strong";
+    public static final String WEAK = "Weak";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Password passObj = new Password(scanner.nextLine());
+        if (passObj.isStrong()){
+            System.out.println(STRONG);
+        }
+        else System.out.println(WEAK);
+
     }
 }
